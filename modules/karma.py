5c498e1b38c4d72c434b7ec9a7d89bc87b3352a0
Brian Shaginaw <bshaginaw120@gmail.com>
2013-08-26 12:59:20 -0400

    Added 'contrib' tracking, to see how active a karmtributor is
    Usage: '.karma contrib _name_'

--- modules/karma.py
+++ modules/karma.py
@@ -8,22 +8,34 @@ import pickle
 
 SHOW_TOP_DEFAULT = 6
 
-def filename(self): 
-   name = self.nick + '-' + self.config.host + '.karma.db'
+def filename(self, fname): 
+   name = ''.join((self.nick, '-', self.config.host, '.', fname, '.db'))
    return os.path.join(os.path.expanduser('~/.phenny'), name)
 
 def setup(self):
     try:
-        f = open(filename(self), "r")
+        f = open(filename(self, "karma"), "r")
         self.karmas = pickle.load(f)
         f.close()
     except IOError:
         self.karmas = {}
+     try:
+        f = open(filename(self, "karma_contrib"), "r")
+        self.karma_contrib = pickle.load(f)
+        f.close()
+     except IOError:
+        self.karma_contrib = {}
 
 def save_karma(self):
     try:
-        f = open(filename(self), "w")
+        f = open(filename(self, "karma"), "w")
         pickle.dump(self.karmas, f)
+        f.close()
+    except IOError:
+        pass
+     try:
+        f = open(filename(self, "karma_contrib"), "w")
+        pickle.dump(self.karma_contrib, f)
         f.close()
     except IOError:
         pass
@@ -32,16 +44,18 @@ def karma_me(phenny, input):
     target = input.group(1).lower()
     karma = (input.group(2) == "++") * 2 - 1
     isself = False
-    if target == input.nick.lower():
+    sender = input.nick.lower()
+    if target == sender:
         isself = True
     if not hasattr(phenny, 'karmas'): 
         return phenny.say('error?')
     if target in phenny.alias_list:
         t = phenny.alias_list[target]
-        if t == input.nick.lower():
+        if t == sender:
             isself = True
-    if input.nick.lower() in phenny.alias_list:
-        if target == phenny.alias_list[input.nick.lower()]:
+    if sender in phenny.alias_list:
+        sender = phenny.alias_list[sender]
+        if target == sender:
             isself = True
     if isself:
         return phenny.say("I'm sorry, "+input.nick+". I'm afraid I can't do that.")
@@ -50,6 +64,9 @@ def karma_me(phenny, input):
             phenny.karmas[target] = karma
         else:
             phenny.karmas[target] += karma
+        if sender not in phenny.karma_contrib:
+            phenny.karma_contrib[sender] = [0, 0]  # +, -
+        phenny.karma_contrib[sender][karma == -1]
         phenny.say(target+"'s karma is now "+str(phenny.karmas[target]))
         save_karma(phenny)
     else:
@@ -60,9 +77,18 @@ def get_karma(phenny, input):
     if not hasattr(phenny, 'karmas'): 
         return phenny.say('error?')
     show_top = input.group(3)
+    contrib = input.group(4)
     if show_top is not None:  # want to show the top x
         nick = None
         show_top = int(show_top)
+    elif contrib is not None:  # want to show how much karma someone's changed
+        lcontrib = contrib.lower()
+        if lcontrib not in phenny.karma_contrib:
+            phenny.say(contrib + " has not altered any karma.")
+            return
+        up, down = map(str, phenny.karma_contrib[lcontrib])
+        phenny.say(' '.join((contrib, "has granted", up, "karma and removed", down, "karma.")))
+        return
     else:
         nick = input.group(2)
         show_top = SHOW_TOP_DEFAULT
@@ -84,7 +110,7 @@ def get_karma(phenny, input):
     else:
         phenny.say("You guys don't have any karma apparently.")
 get_karma.name = 'karma'
-get_karma.rule = r'^\.(karma)(?: (top (\d)|\S+))?$'
+get_karma.rule = r'^\.(karma)(?: (top (\d)|contrib (\S+)|\S+))?$'
 
 def nuke_karma(phenny, input):
     if input.nick not in phenny.ident_admin: return phenny.notice(input.nick, 'Requires authorization. Use .auth to identify')
