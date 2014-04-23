4a07ecd63fef32573eee15519e7a08230958ea02
Brian Shaginaw <brian.shaginaw@warbyparker.com>
2014-04-23 17:51:28 -0400

    Oh.

--- modules/karma.py
+++ modules/karma.py
@@ -320,9 +320,10 @@ def stupid_test(phenny, input):
     """Stupid friggin test."""
     if input.group(2) is None:
         phenny.say("Incredibly stupid.")
-    phenny.say('stupid: ' + input.group(2))
+    else:
+        phenny.say('stupid: ' + input.group(2))
 stupid_test.name = 'karma stupid'
-stupid_test.rule = (['karma'], r'stupid +(\d)$')
+stupid_test.rule = ('karma', r'stupid +(\d)$')
 
 def set_primary_alias(phenny, input):
     """Set your primary alias, to be displayed in the karma rankings"""
